4e027bd835c6ba5933943f6c8b194212f9307edc
Armen <armen.baghdasaryan91@gmail.com>
2022-11-12 15:03:05 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { Projects } from './pages/Projects/Projects'
+import { AboutCompany } from './pages/AboutCompany/AboutCompany'
+import { Contacts } from './pages/Contacts/Contacts'
+import { Navbar } from './components/Navbar/Navbar'
+import { Footer } from './components/Footer/Footer'
+import 'react-toastify/dist/ReactToastify.css';
+import './App.scss'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path='/projects' element={<Projects />} />
+        <Route exact path='/company' element={<AboutCompany />} />
+        <Route exact path='/contacts' element={<Contacts />} />
+        <Route path="/" element={<Navigate to="/contacts" />} />
+      </Routes>
+      <Footer />
+    </>
+
+  )
 }
 
-export default App;
